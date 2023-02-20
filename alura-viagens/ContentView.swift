e49137a74544064d68849484d8b0565a7ad5d659
mihcristina <contato@michellicristina.com.br>
2023-02-20 12:45:47 -0300

    refactor screens into separate files

--- alura-viagens/ContentView.swift
+++ alura-viagens/ContentView.swift
@@ -11,76 +11,17 @@ struct ContentView: View {
     var body: some View {
 
         GeometryReader { view in
-
             VStack {
-                VStack {
-                    Text("alura viagens")
-                        .font(.custom("Avenir Black", size: 20))
-                        .foregroundColor(Color.white)
-                        .padding(.top, 50)
-                    Text("ESPECIAL")
-                        .foregroundColor(Color.white)
-                        .font(.custom("Avenir Book", size: 20))
-                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, 30)
-                    Text("BRASIL")
-                        .foregroundColor(Color.white)
-                        .font(.custom("Avenir Black", size: 23))
-                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, 30)
-                }
-                .frame(width: view.size.width, height: 180, alignment: .top)
-                .background(Color.purple)
-
-                HStack {
-
-                    Button(action: {}) {
-                        Text("Hotéis")
-                            .font(.custom("Avenir Medium", size: 17))
-                            .foregroundColor(.white)
-                    }
-                    .frame(width: 100, height: 50)
-                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 10))
-                    .background(Color.blue)
-                    .offset(x: 50)
-
-                    Spacer()
-
-                    Button(action: {}) {
-                        Text("Pacotes")
-                            .font(.custom("Avenir Medium", size: 17))
-                            .foregroundColor(.white)
-                    }
-                    .frame(width: 100, height: 50)
-                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.orange, lineWidth: 10))
-                    .background(Color.orange)
-                    .offset(x: -50)
-                }
-                .offset(y: -30)
-
+                HeaderView()
+                    .frame(width: view.size.width, height: 200, alignment: .top)
                 List(viagens) { viagem in
-                    VStack(alignment: .leading) {
-                        Text(viagem.titulo)
-                        Image(viagem.imagem)
-                            .resizable()
-                            .frame(height: 125)
-
-                        HStack {
-                            Text(viagem.quantidadeDeDias)
-
-                            Spacer()
-
-                            Text(viagem.valor)
-                        }
-
-                    }
+                    CelulaViagemView(viagem: viagem)
                 }
+            }
                 .scrollContentBackground(.hidden)
             }
         }
-        .edgesIgnoringSafeArea(.all)
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ alura-viagens/Views/HeaderView.swift
@@ -0,0 +1,71 @@
+//
+//  eaderView
+//  alura-viagens
+//
+//  Created by Michelli Cristina de Paulo Lima on 20/02/23.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+
+        GeometryReader { view in
+            VStack {
+
+                VStack {
+                    VStack {
+                        Text("alura viagens")
+                            .font(.custom("Avenir Black", size: 20))
+                            .foregroundColor(Color.white)
+                            .padding(.top, 50)
+                        Text("ESPECIAL")
+                            .foregroundColor(Color.white)
+                            .font(.custom("Avenir Book", size: 20))
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 30)
+                        Text("BRASIL")
+                            .foregroundColor(Color.white)
+                            .font(.custom("Avenir Black", size: 23))
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 30)
+                    }
+                    .frame(width: view.size.width, height: 180, alignment: .top)
+                    .background(Color.purple)
+
+                    HStack {
+
+                        Button(action: {}) {
+                            Text("Hotéis")
+                                .font(.custom("Avenir Medium", size: 17))
+                                .foregroundColor(.white)
+                        }
+                        .frame(width: 100, height: 50)
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 10))
+                        .background(Color.blue)
+                        .offset(x: 50)
+
+                        Spacer()
+
+                        Button(action: {}) {
+                            Text("Pacotes")
+                                .font(.custom("Avenir Medium", size: 17))
+                                .foregroundColor(.white)
+                        }
+                        .frame(width: 100, height: 50)
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.orange, lineWidth: 10))
+                        .background(Color.orange)
+                        .offset(x: -50)
+                    }
+                    .offset(y: -30)
+                }
+            }
+        }
+    }
+
+    struct SwiftUIView_Previews: PreviewProvider {
+        static var previews: some View {
+            HeaderView()
+        }
+    }
+}
