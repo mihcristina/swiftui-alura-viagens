c185beea66ca78c425fbf848a4cfa47c9fe99658
mihcristina <contato@michellicristina.com.br>
2023-02-20 11:17:28 -0300

    Fix padding header

--- alura-viagens/ContentView.swift
+++ alura-viagens/ContentView.swift
@@ -9,28 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("alura viagens")
-                .font(.custom("Avenir Black", size: 20))
-                .foregroundColor(Color.white)
-            Text("ESPECIAL")
-                .foregroundColor(Color.white)
-                .font(.custom("Avenir Book", size: 20))
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 30)
-            Text("BRASIL")
-                .foregroundColor(Color.white)
-                .font(.custom("Avenir Black", size: 23))
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 30)
-            List {
-                Text("Rio de Janeiro")
-                Text("Ceará")
-                Text("Atibaia")
-                Text("Rio de Janeiro")
+
+        GeometryReader { view in
+
+            VStack {
+                VStack {
+                    Text("alura viagens")
+                        .font(.custom("Avenir Black", size: 20))
+                        .foregroundColor(Color.white)
+                    Text("ESPECIAL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    Text("BRASIL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 23))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(Color.purple)
+                List {
+                    Text("Rio de Janeiro")
+                    Text("Rio de Janeiro")
+                    Text("Rio de Janeiro")
+                    Text("Rio de Janeiro")
+                }
             }
         }
-        .background(Color.purple)
     }
 }
 
