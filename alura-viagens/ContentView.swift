9a947fc1fa04ad3d2b6451a8ea5b6483554041c0
mihcristina <contato@michellicristina.com.br>
2023-02-20 11:46:50 -0300

    fix buttons cornerRadius

--- alura-viagens/ContentView.swift
+++ alura-viagens/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
                     Text("alura viagens")
                         .font(.custom("Avenir Black", size: 20))
                         .foregroundColor(Color.white)
+                        .padding(.top, 50)
                     Text("ESPECIAL")
                         .foregroundColor(Color.white)
                         .font(.custom("Avenir Book", size: 20))
@@ -30,6 +31,33 @@ struct ContentView: View {
                 }
                 .frame(width: view.size.width, height: 180, alignment: .top)
                 .background(Color.purple)
+
+                HStack {
+
+                    Button(action: {}) {
+                        Text("Hotéis")
+                            .font(.custom("Avenir Medium", size: 17))
+                            .foregroundColor(.white)
+                    }
+                    .frame(width: 100, height: 50)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 10))
+                    .background(Color.blue)
+                    .offset(x: 50)
+
+                    Spacer()
+
+                    Button(action: {}) {
+                        Text("Pacotes")
+                            .font(.custom("Avenir Medium", size: 17))
+                            .foregroundColor(.white)
+                    }
+                    .frame(width: 100, height: 50)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.orange, lineWidth: 10))
+                    .background(Color.orange)
+                    .offset(x: -50)
+                }
+                .offset(y: -30)
+
                 List {
                     Text("Rio de Janeiro")
                     Text("Rio de Janeiro")
@@ -38,6 +66,7 @@ struct ContentView: View {
                 }
             }
         }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
